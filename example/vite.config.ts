053ebe33454d3eb5d90654da825278a2e0a1847b
pchalupa <chalupa.petr93@gmail.com>
2025-01-07 10:15:58 +0100

    feat: add button component

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,7 +1,13 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	plugins: [TanStackRouterVite({}), react()],
+	plugins: [TanStackRouterVite({}), react(), tailwindcss()],
+	test: {
+		globals: true,
+		environment: 'jsdom',
+	},
 });

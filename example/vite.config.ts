eeb0c77d11f74a56bb5ef923409fac2b4463dbdf
pchalupa <chalupa.petr93@gmail.com>
2025-02-16 13:12:06 +0100

    chore(deps): update vitest

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [TanStackRouterVite({}), react(), tailwindcss()],
+	plugins: [TanStackRouterVite(), react(), tailwindcss()],
 	test: {
 		globals: true,
 		environment: "jsdom",

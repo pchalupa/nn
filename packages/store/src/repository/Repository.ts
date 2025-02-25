ed09a3133aa02e89db8999fde93d978e5833949f
pchalupa <chalupa.petr93@gmail.com>
2025-02-25 13:04:49 +0100

    feat: repository package

--- packages/store/src/repository/Repository.ts
+++
@@ -1,7 +0,0 @@
-import { EventEmitter } from "../EventEmitter";
-
-export abstract class Repository extends EventEmitter<{ didSet: [id: string, value: unknown] }> {
-	abstract get(id: unknown): unknown | undefined;
-
-	abstract set(id: unknown, value: unknown): void;
-}

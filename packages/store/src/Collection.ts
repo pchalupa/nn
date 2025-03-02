73ed040f55bc1d44eac16954cfaa6c11e5e1d0a8
pchalupa <chalupa.petr93@gmail.com>
2025-03-02 21:37:23 +0100

    refactor: collection

--- packages/store/src/Collection.ts
+++ packages/store/src/Collection.ts
@@ -2,47 +2,42 @@ import { EventEmitter } from "@nn/event-emitter";
 import type { Repository } from "@nn/repository";
 import { Reference } from "./Reference";
 
-// TOOD: Consider implementing dispose and clean up events
-export class Collection<Type> extends EventEmitter<{ update: [] }> {
+export class Collection<Value extends { id: string }> extends EventEmitter<{ update: [] }> {
 	constructor(
-		private data: Reference[] = [],
-		private repository?: Repository,
+		private data: Reference<Value>[] = [],
+		private repository?: Repository<Value>,
 	) {
 		super();
 
-		if (this.repository) {
-			this.repository.on("didSet", (id: string) => {
-				const reference = Reference.createReferenceFor(() => this.repository.get(id));
+		this.repository?.on("didSet", (id: string) => {
+			const reference = Reference.createReferenceFor(() => this.repository?.get(id));
 
-				this.data.push(reference);
-			});
-		}
+			this.data.push(reference);
+		});
 	}
 
-	get length() {
+	get length(): number {
 		return this.data.length;
 	}
 
-	toString() {
+	toString(): string {
 		return this.data.toString();
 	}
 
-	push(value: Type) {
-		const id = value.id;
-
-		this.repository?.set(id, value);
+	push(value: Value): void {
+		this.repository?.set(value.id, value);
 		this.emit("update");
 	}
 
-	map<T>(callback: (value: Type, index: number) => T): T[] {
+	map<Type>(callback: (value: Reference<Value>, index: number) => Type): Type[] {
 		const data = this.data.map(callback);
 
 		return data;
 	}
 
-	filter(predicate: (data: Type) => boolean): Collection<Type> {
+	filter(predicate: (data: Reference<Value>) => boolean): Collection<Value> {
 		const data = this.data.filter(predicate);
-		const collection = new Collection(data);
+		const collection = new Collection<Value>(data);
 
 		collection.repository = this.repository;
 

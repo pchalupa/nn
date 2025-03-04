69261c311cc76428a7d413cc73ea499a6cfec25e
pchalupa <chalupa.petr93@gmail.com>
2025-03-04 10:25:45 +0100

    feat: backlog route

--- example/src/routes/__root.tsx
+++ example/src/routes/__root.tsx
@@ -1,6 +1,30 @@
-import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
+import { useStore } from "../store";
 
-const RootComponent = () => <Outlet />;
+const Stats = () => {
+	const data = useStore((store) => store.tickets);
+
+	return (
+		<div className="flex flex-col gap-y-2">
+			<h3 className="text-sm uppercase">Stats</h3>
+			<hr className="border-sky-600" />
+			<div className="flex flex-col gap-y-2">{data.length}</div>
+		</div>
+	);
+};
+
+const RootComponent = () => (
+	<div className="flex flex-row">
+		<div className="min-h-screen bg-sky-100">
+			<Link to="/">
+				<h1 className="p-4 font-bold text-2xl">Kanban Board</h1>
+			</Link>
+			<Stats />
+			<Link to="/backlog">Backlog</Link>
+		</div>
+		<Outlet />
+	</div>
+);
 
 export const Route = createRootRoute({
 	component: RootComponent,
--- example/src/routes/index.tsx
+++ example/src/routes/index.tsx
@@ -1,34 +1,13 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Column } from "../components/Column";
-import { useStore } from "../store";
 
-const Stats = () => {
-	const data = useStore((store) => store.tickets);
-
-	return (
-		<div className="flex flex-col gap-y-2">
-			<h3 className="text-sm uppercase">Stats</h3>
-			<hr className="border-sky-600" />
-			<div className="flex flex-col gap-y-2">{data.length}</div>
-		</div>
-	);
-};
-
-const Index = () => {
-	return (
-		<div className="flex flex-row">
-			<div className="min-h-screen bg-sky-100">
-				<h1 className="p-4 font-bold text-2xl">Kanban Board</h1>
-				<Stats />
-			</div>
-			<div className="flex h-full flex-1 flex-row gap-x-2 p-4">
-				<Column title="To Do" status="todo" />
-				<Column title="In Progress" status="in-progress" />
-				<Column title="Done" status="done" />
-			</div>
-		</div>
-	);
-};
+const Index = () => (
+	<div className="flex h-full flex-1 flex-row gap-x-2 p-4">
+		<Column title="To Do" status="todo" />
+		<Column title="In Progress" status="in-progress" />
+		<Column title="Done" status="done" />
+	</div>
+);
 
 export const Route = createFileRoute("/")({
 	component: Index,

a7d4a720e8c9772028e37518c09e99684054a5f7
pchalupa <chalupa.petr93@gmail.com>
2025-03-04 10:27:51 +0100

    fix: import

--- example/src/routeTree.gen.ts
+++ example/src/routeTree.gen.ts
@@ -11,7 +11,7 @@
 // Import Routes
 
 import { Route as rootRoute } from './routes/__root'
-import { Route as BacklogImport } from '././routes/backlog
+import { Route as BacklogImport } from './routes/backlog'
 import { Route as IndexImport } from './routes/index'
 
 // Create/Update Routes

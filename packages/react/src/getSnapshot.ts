79fbdf50ed4f93743a16c65a69702024252d4936
pchalupa <chalupa.petr93@gmail.com>
2025-03-05 21:21:34 +0100

    fix: add type

--- packages/react/src/getSnapshot.ts
+++ packages/react/src/getSnapshot.ts
@@ -1,6 +1,9 @@
 import type { Store } from "@nn/store";
 import type { Selector } from ".";
 
-export function getSnapshot<Schema extends object>(selector: Selector<Schema>, store: Store<Schema>) {
-	return () => store.getSnapshotOf(selector).id;
+export function getSnapshot<Schema extends object>(
+	selector: Selector<Schema>,
+	store: Store<Schema>,
+): () => string | undefined {
+	return () => store.getSnapshotOf<ReturnType<Selector<Schema>>>(selector)?.id;
 }

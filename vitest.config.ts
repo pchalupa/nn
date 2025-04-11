3fea80556b183b3d76a6890f7e5e3f5af63877c8
Petr Chalupa <chalupa.petr93@gmail.com>
2025-04-11 10:29:15 +0200

    chore: setup example server (#32)

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
-		workspace: ["packages/*", "example"],
+		workspace: ["packages/*", "apps/*"],
 		coverage: {
 			exclude: ["**/coverage", "**/dist", "**/*.config.*", "**/*.gen.*"],
 		},

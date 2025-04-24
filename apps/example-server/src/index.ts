6a622132371859a730915d2cfd3bf3786af60458
Petr Chalupa <chalupa.petr93@gmail.com>
2025-04-24 08:20:24 +0200

    feat(example): server simple persistence (#34)

--- apps/example-server/src/index.ts
+++ apps/example-server/src/index.ts
@@ -1,9 +1,35 @@
+import cors from "cors";
 import express from "express";
+import * as db from "./services/db/db";
 
 const app = express();
 
-app.get("/", (_req, res) => {
-	res.send("Hello World");
+app.use(cors());
+app.use(express.json());
+
+app.get("/pull", async (_req, res) => {
+	try {
+		const data = await db.get();
+
+		res.json(data);
+	} catch (error) {
+		res.status(500).json({ error: "Failed to fetch data" });
+	}
+});
+
+app.post("/push", async (req, res) => {
+	try {
+		const data = req.body;
+
+		if (!data || typeof data !== "object") res.status(400).json({ error: "No data provided" });
+		else {
+			await db.update(data);
+
+			res.json(data);
+		}
+	} catch (error) {
+		res.status(500).json({ error: "Failed to update data" });
+	}
 });
 
 app.listen(process.env.PORT);
---
+++ apps/example-server/src/services/db/db.ts
@@ -0,0 +1,42 @@
+import { join } from "path";
+import { readFile, writeFile } from "fs/promises";
+
+const database = join(__dirname, "db.json");
+
+export async function update(data: Record<string, unknown>): Promise<void> {
+	try {
+		const db = await readDb();
+
+		await writeDb(Object.assign(db, data));
+	} catch (_error) {
+		throw new Error("Failed to update data");
+	}
+}
+
+export async function get(): Promise<Record<string, unknown>> {
+	try {
+		const db = await readDb();
+
+		return db;
+	} catch (_error) {
+		throw new Error("Failed to get data");
+	}
+}
+
+async function readDb(): Promise<Record<string, unknown>> {
+	try {
+		const db = await readFile(database, "utf-8");
+
+		return JSON.parse(db);
+	} catch (_error) {
+		throw new Error("Failed to read database");
+	}
+}
+
+async function writeDb(data: Record<string, unknown>): Promise<void> {
+	try {
+		await writeFile(database, JSON.stringify(data, null, 2), "utf-8");
+	} catch (_error) {
+		throw new Error("Failed to write database");
+	}
+}

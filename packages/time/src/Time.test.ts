e2760030cce278a90bf88e6195e85e663c38cdb6
pchalupa <chalupa.petr93@gmail.com>
2025-05-12 22:09:09 +0200

    refactor: rename package

--- packages/time/src/Time.test.ts
+++ packages/time/src/Time.test.ts
@@ -1,5 +1,5 @@
 import { afterAll, beforeAll, describe, expect, it, vi } from "vitest";
-import { Time } from "./index";
+import { Time } from "./Time";
 
 describe("Time", () => {
 	beforeAll(() => {

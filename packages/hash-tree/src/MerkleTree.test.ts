4d34ceb8ea943235c8c57c30638f5e7ba92966e2
pchalupa <chalupa.petr93@gmail.com>
2025-07-24 18:21:55 +0200

    fix import

--- packages/hash-tree/src/MerkleTree.test.ts
+++ packages/hash-tree/src/MerkleTree.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { MerkleTree } from "./index";
+import { MerkleTree } from "./MerkleTree";
 
 describe("MerkleTree", () => {
 	it("should create a Merkle tree", async () => {

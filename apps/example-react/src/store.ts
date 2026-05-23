9387fa899e9a529a3fe69b6190eb8327e6c7ddb8
pchalupa <chalupa.petr93@gmail.com>
2026-05-23 22:04:56 +0200

    Use new schema

--- apps/example-react/src/store.ts
+++ apps/example-react/src/store.ts
@@ -1,36 +1,34 @@
 import { HttpRemote } from "@nn/http-remote";
 import { IndexDbRepository } from "@nn/indexdb-repository";
 import { createStore, use } from "@nn/react";
-import { collection } from "@nn/schema";
+import { array, object, string } from "@nn/schema";
 
-type Ticket = {
-	id: string;
-	title: string;
-	description: string;
-	status: "todo" | "in-progress" | "done";
-	assignee?: User;
-};
+const user = object({
+	name: string().describe("User name"),
+	email: string(),
+})
+	.identify("user")
+	.describe("Holds information about a user.");
 
-type User = {
-	id: string;
-	name: string;
-	email: string;
-};
-
-type Project = {
-	id: string;
-	name: string;
-};
+const ticket = object({
+	title: string(),
+	description: string(),
+	status: string(),
+	assignee: user,
+})
+	.identify("ticket")
+	.describe("Holds information about a ticket.");
 
 const remote = new HttpRemote(import.meta.env.VITE_REMOTE_URL);
+const schema = object({
+	users: array(user).describe("List of users"),
+	tickets: array(ticket).describe("List of tickets"),
+});
+
 const store = createStore({
 	repository: IndexDbRepository,
 	remote,
-	schema: {
-		users: collection<User>(),
-		projects: collection<Project>(),
-		tickets: collection<Ticket>(),
-	},
+	schema,
 });
 
 export const useStore = use(store);

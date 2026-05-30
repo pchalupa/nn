1eddf6e1dbd58ed2fbecd3de4cf108a70aa5c959
pchalupa <chalupa.petr93@gmail.com>
2026-05-30 16:16:15 +0200

    Add init method

--- packages/repository/src/Repository.ts
+++ packages/repository/src/Repository.ts
@@ -3,6 +3,8 @@ export interface RepositoryFactory {
 }
 
 export interface Repository {
+	init(schema: Record<string, unknown>): Promise<void>;
+
 	set<Value>(id: string, value: Value, typeName: string): Promise<void>;
 	getAll<Value>(typeName: string): Promise<Value[]>;
 }

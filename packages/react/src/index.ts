c3848a589ecc0e7519947ccacbc7110459c6d6da
pchalupa <chalupa.petr93@gmail.com>
2026-09-21 21:52:19 +0200

    Fix lint issue

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,6 +1,6 @@
+import type { Observable } from "@nn/event-emitter/Observable";
 import type { Remote } from "@nn/remote";
 import type { Repository } from "@nn/repository";
-import type { Observable } from "@nn/event-emitter/Observable";
 import type { ObjectSchema, Schema } from "@nn/schema";
 import { Store } from "@nn/store";
 import { useDebugValue, use as usePromise, useRef, useSyncExternalStore } from "react";
@@ -8,18 +8,18 @@ import { useDebugValue, use as usePromise, useRef, useSyncExternalStore } from "
 import { getSnapshot } from "./getSnapshot";
 import { subscribe } from "./subscribe";
 
-export type Selector<Schema, Snapshot extends Observable = Observable> = (store: Schema) => Snapshot;
+export type Selector<StoreSchema, Snapshot extends Observable = Observable> = (store: StoreSchema) => Snapshot;
 
-export async function createStore<StoreSChema extends ObjectSchema<Record<string, Schema>>>(options: {
-	schema: StoreSChema;
+export async function createStore<StoreSchema extends ObjectSchema<Record<string, Schema>>>(options: {
+	schema: StoreSchema;
 	repository?: Repository;
 	remote?: Remote;
 }) {
 	return Store.fromSchema(options);
 }
 
-export function use<Schema extends object>(promisedStore: Promise<Store<Schema>>) {
-	return function useStore<Type extends Observable>(selector: Selector<Schema, Type>) {
+export function use<StoreSchema extends object>(promisedStore: Promise<Store<StoreSchema>>) {
+	return function useStore<Type extends Observable>(selector: Selector<StoreSchema, Type>) {
 		const store = usePromise(promisedStore);
 		const selectorRef = useRef(selector);
 		const subscribeRef = useRef(subscribe(store));

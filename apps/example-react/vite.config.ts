c4f354ada573eddc46052337d8bfbe0ffbe0fd39
Petr Chalupa <chalupa.petr93@gmail.com>
2026-09-21 12:11:39 +0200

    Update dependencies (#72)

--- apps/example-react/vite.config.ts
+++ apps/example-react/vite.config.ts
@@ -1,22 +1,24 @@
+import Babel from "@rolldown/plugin-babel";
 import TailwindCSS from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
-import React from "@vitejs/plugin-react";
+import React, { reactCompilerPreset } from "@vitejs/plugin-react";
 /// <reference types="vitest" />
 import { defineConfig, loadEnv } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 const reactCompilerConfig = {
-	target: "19",
+	target: "19" as const,
 };
 
 export default defineConfig(({ mode }) => ({
 	plugins: [
 		VitePWA({ registerType: "autoUpdate", devOptions: { enabled: true } }),
 		tanstackRouter(),
-		React({ babel: { plugins: [["babel-plugin-react-compiler", reactCompilerConfig]] } }),
+		React(),
+		Babel({ presets: [reactCompilerPreset(reactCompilerConfig)] }),
 		TailwindCSS(),
 	],
-	esbuild: {
+	oxc: {
 		target: "es2022",
 	},
 	test: {
